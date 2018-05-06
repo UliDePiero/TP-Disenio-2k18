90f58ec02ec252703a8576af52811c37a9c14ede
lpanizza@est.frba.utn.edu.ar <lpanizza@est.frba.utn.edu.ar>
2018-05-06 02:01:18 -0300

    misc

---
+++ TP0/TP0/Helpers/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP0.Helpers
+{
+    public class Categoria
+    {
+        public int consumoMin;
+        public int consumoMax;
+        public float cargoFijo;      //  $/mes
+        public float cargoVariable;  //  $/kWh
+    }
+}
