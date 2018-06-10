7dcf041373acf2f092a961e0fdf501b5ccf350a2
Nicolas <ncoen97@gmail.com>
2018-06-09 17:57:08 -0700

    Actualizado a diagrama con comportamientos

---
+++ TP0/TP0/Helpers/Actuador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP0.Helpers
+{
+    public class Actuador
+    {
+        public List<Regla> Reglas;
+        public void verificarRegla()
+        {
+            if (Reglas.All(c=>c.seCumple))
+            {
+                ejecutarRegla();
+            }
+        }
+        public void ejecutarRegla()
+        {
+            
+        }
+    }
+}
--- TP0/TP0/Helpers/Sensor.cs
+++ TP0/TP0/Helpers/Sensor.cs
@@ -8,18 +8,51 @@ namespace TP0.Helpers
     public class Sensor
     {
         public float valor;
-        public List<Comportamiento> observers;
-        public void agregarObservador(Comportamiento c)
+        public List<Regla> observers;
+        public void agregarObservador(Regla c)
         {
             observers.Add(c);
         }
-        public void quitarObservador(Comportamiento c)
+        public void quitarObservador(Regla c)
         {
             observers.Remove(c);
         }
         public void notificar()
         {
             observers.ForEach(o => o.notificar(valor));
+        }
+        public void censar()
+        {
+
+        }
+    }
+
+    public class SensorHumedad : Sensor
+    {
+        new public void censar()
+        {
+
+        }
+    }
+    public class SensorMovimiento : Sensor
+    {
+        new public void censar()
+        {
+
+        }
+    }
+    public class SensorTemperatura : Sensor
+    {
+        new public void censar()
+        {
+
+        }
+    }
+    public class SensorLuz : Sensor
+    {
+        new public void censar()
+        {
+
         }
     }
 }
