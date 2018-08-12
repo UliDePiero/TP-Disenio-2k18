35d00c92898b4e8b2f657c79fbb95fbbf63152d7
FrodriguezSandez <frodriguezsandez@hotmail.com>
2018-08-12 17:05:43 -0300

    Update Simplex.cs

--- TP0/TP0/Helpers/Static/Simplex/Simplex.cs
+++ TP0/TP0/Helpers/Static/Simplex/Simplex.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +9,7 @@ namespace TP0.Helpers.Static.Simplex
     {
         public List<Double> vars { get; set; }
         public List<restriction> restrictions{ get; set; }
+
         public List<Double> getVars(int cantidad)
         {
             List<Double> valores = new List<Double>(50);
@@ -19,6 +20,30 @@ namespace TP0.Helpers.Static.Simplex
                 i++;
             }
             return valores;
+        }
+
+        public List<Double> generarValues(int cantidadDispos, int ubicacionDispositivo, double max1min0, double valorDeReferencia)
+        {
+            List<Double> valores = new List<Double>(cantidadDispos+2); //uno por cada dispositivo que haya + el valor de referencia + si es max o min
+
+            valores.Add(valorDeReferencia); // que es el primero
+            int i = 1;
+
+            for (; i < cantidadDispos; i++) 
+            { 
+                if (i != ubicacionDispositivo)
+                {
+                    valores.Add(0);
+
+                } else {
+                    valores.Add(1);
+
+                }
+             }
+
+            valores.Add(max1min0); // este ultimo campo se saca desde la restriccion
+
+            return valores; //esta lista es del tipo "values":[90,0,0,1....]
         }
     }
 }
