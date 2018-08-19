afd881e3a7a22de2902df45367f7f41656c8b5ff
FrodriguezSandez <frodriguezsandez@hotmail.com>
2018-08-19 00:08:06 -0300

    Update SimplexHelper.cs

--- TP0/TP0/Helpers/Static/Simplex/SimplexHelper.cs
+++ TP0/TP0/Helpers/Static/Simplex/SimplexHelper.cs
@@ -14,7 +14,7 @@ namespace TP0.Helpers.Static.Simplex
         {
             var obj = new Simplex();//obj es el objeto q se pasa a json
             int dispTotales = estandars.Count() + inteligentes.Count();
-            List<int> v = obj.getVars(dispTotales); //se hace la primera fila del archivo json
+            List<Double> v = obj.getVars(dispTotales); //se hace la primera fila del archivo json
             obj.vars = v;
             List<Double> values1 = new List<Double>(); //esta es la primera fila de valores de la parte de "restricciones"
             values1.Add(620000); //consumo total = 620 // Lo subi a 620000 porque sino no anda por la cantidad de dispositivos
